463ade1016a959dc163487b259627c0a3bf7c1a2
Nokuvimba <G00423951@atu.ie>
2023-12-04 10:49:00 +0000

    Added a part2 object to set methods and display the details using the getters.

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,13 +1,23 @@
 package ie.atu;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
 Person myPerson = new Person("Natty","Harare","911");
         System.out.println(myPerson.toString());
-        Customer myCustomer= new Customer("Noku","Mutare","+263","Fifi",true);
-        System.out.println(myCustomer.toString());
-        Scanner keyboard= new Scanner(System.in);
+        Customer Part1 = new Customer("Natasha","Kadoma","+353","1",true);
+        System.out.println(Part1.toString());
+        Customer Part2 = new Customer();
+        Part2.setName("Nokuvimba");
+        Part2.setAddress("Galway");
+        Part2.setTelephone("096546");
+        Part2.setCustomerNumber("2");
+        Part2.setMailingList(false);
 
+        System.out.println("Name:"+Part2.getName());
+        System.out.println("Address:"+Part2.getAddress());
+        System.out.println("Telephone:"+Part2.getTelephone());
+        System.out.println("CustomerNumber:"+Part2.getName());
+        System.out.println("MailingList:"+Part2.isMailingList());
     }
 }
